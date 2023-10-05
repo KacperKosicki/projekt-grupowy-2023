3096561e5c37e2086908df380627e1c621f2bb3f
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-10-05 20:47:00 +0200

    Add code and styles for dropdown category selection

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -10,9 +10,18 @@ const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
+      {/*<select name='' id=''>
         <option value=''>Select a category</option>
-      </select>
+        </select> */}
+      <div className={styles.customSelect}>
+        <span className={styles.selectCategory}>Select Category</span>
+        <ul>
+          <li className={styles.listCategory}>Bestsellers</li>
+          <li className={styles.listCategory}>Brand new</li>
+          <li className={styles.listCategory}>Sale</li>
+          <li className={styles.listCategory}>Inspirations</li>
+        </ul>
+      </div>
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
