8a15553a4be38ef6eea1ef5296f92c18649bccca
Kacper Kosicki <kosickikacper1@gmail.com>
2023-10-06 10:48:35 +0200

    Add a hover for buttons and change the price background at the same time

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
