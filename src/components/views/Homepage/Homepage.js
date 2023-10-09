1ce91923449a5351687869a81bef327aeb12f2d6
Tomasz Cichon <tomekcichon@tlen.pl>
2023-10-09 16:37:03 +0200

    add Promo Component to HomePage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,10 +5,12 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import Promo from '../../features/Promo/Promo';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
+    <Promo />
     <NewFurniture />
   </div>
 );
