10e8b559d78d3fa5543740d19d96b82abf8c0f9b
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-10-09 21:22:43 +0200

    WDP231003-20

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import initialState from './initialState';
 import cartReducer from './cartRedux';
 import categoriesReducer from './categoriesRedux';
 import productsReducer from './productsRedux';
+import screenModeReducer from './screenModeRedux';
 
 // define reducers
 const reducers = {
   cart: cartReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  screenMode: screenModeReducer,
 };
 
 // add blank reducers for initial state properties without reducers
