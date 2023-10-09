6967f54ecaf2c55a83c94521e7fd26531bd8ec4a
Tomasz Cichon <tomekcichon@tlen.pl>
2023-10-09 17:33:30 +0200

    change component name from Promo to Sales for clarification

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,12 +5,12 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
-import Promo from '../../features/Promo/Promo';
+import Sales from '../../features/Sales/Sales';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
-    <Promo />
+    <Sales />
     <NewFurniture />
   </div>
 );
