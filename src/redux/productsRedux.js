192c346c08342c2dc6bf762846c758c69514665b
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-10-10 17:21:49 +0200

    fix more conflicts in other files

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -13,11 +13,16 @@ const reducerName = 'products';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 /* action types */
+const TOGGLE_PRODUCT_FAVORITE = createActionName('TOGGLE_PRODUCT_FAVORITE');
 const TOGGLE_TO_COMPARE = createActionName('TOGGLE_TO_COMPARE');
 const REMOVE_FROM_COMPARISON = createActionName('REMOVE_FROM_COMPARISON');
 const INITIALIZE_PRODUCTS = createActionName('INITIALIZE_PRODUCTS');
 
 /* action creators */
+export const toggleProductFavorite = payload => ({
+  type: TOGGLE_PRODUCT_FAVORITE,
+  payload,
+});
 export const toggleToCompare = payload => ({ type: TOGGLE_TO_COMPARE, payload });
 export const removeFromComparison = payload => ({
   type: REMOVE_FROM_COMPARISON,
@@ -28,6 +33,12 @@ export const initializeProducts = () => ({ type: INITIALIZE_PRODUCTS });
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_PRODUCT_FAVORITE:
+      return statePart.map(product =>
+        product.id === action.payload
+          ? { ...product, isFavorite: !product.isFavorite }
+          : product
+      );
     case TOGGLE_TO_COMPARE:
       return statePart.map(product =>
         product.id === action.payload
