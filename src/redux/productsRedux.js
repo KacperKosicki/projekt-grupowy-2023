512a7d20eaf33311500eea174d3600c5845a0cd7
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-10-10 15:40:58 +0200

    fix all problems with comparison, add component CompareStickyBar with styles

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -13,6 +13,8 @@ const createActionName = actionName => `app/products/${actionName}`;
 const TOGGLE_PRODUCT_FAVORITE = createActionName('TOGGLE_PRODUCT_FAVORITE');
 const TOGGLE_TO_COMPARE = createActionName('TOGGLE_TO_COMPARE');
 const UPDATE_PRODUCT_STARS = createActionName('UPDATE_PRODUCT_STARS');
+const REMOVE_FROM_COMPARISON = createActionName('REMOVE_FROM_COMPARISON');
+const INITIALIZE_PRODUCTS = createActionName('INITIALIZE_PRODUCTS');
 
 /* action creators */
 export const toggleProductFavorite = payload => ({
@@ -20,6 +22,11 @@ export const toggleProductFavorite = payload => ({
   payload,
 });
 export const toggleToCompare = payload => ({ type: TOGGLE_TO_COMPARE, payload });
+export const removeFromComparison = payload => ({
+  type: REMOVE_FROM_COMPARISON,
+  payload,
+});
+export const initializeProducts = () => ({ type: INITIALIZE_PRODUCTS });
 
 export const updateProductStars = payload => ({
   type: UPDATE_PRODUCT_STARS,
@@ -41,11 +48,17 @@ export default function reducer(statePart = [], action = {}) {
           ? { ...product, comparison: !product.comparison }  
           : product
       );
+      case REMOVE_FROM_COMPARISON:
+      return statePart.map(product =>
+        product.id === action.payload ? { ...product, comparison: false } : product
+      );
+    case INITIALIZE_PRODUCTS:
+      return statePart.map(product => ({ ...product, comparison: false })); // Ustawiam wszystkie produkty na false
     case UPDATE_PRODUCT_STARS:
       return statePart.map(product =>
         product.id === action.payload.id
           ? { ...product, userStars: action.payload.userStars }
-          :product);  
+          :product);
     default:
       return statePart;
   }
