f6a0d819bfebef24c2cc09d52d30346658b2db2f
Tomasz Cichon <tomekcichon@tlen.pl>
2023-10-13 09:09:46 +0200

    remove shop component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPage';
-import Shop from './components/views/Shop/Shop';
 
 const App = () => (
   <Provider store={store}>
@@ -18,7 +17,6 @@ const App = () => (
       <MainLayout>
         <Switch>
           <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/'} component={Shop} />
           <Route exact path={'/shop/:categoryId'} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
         </Switch>
