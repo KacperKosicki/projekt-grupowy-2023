d1ccea01791b1f236773a1801c4a7008c91e5d77
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-10-14 18:34:22 +0200

    Add gallery draft layout

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -3,13 +3,16 @@ import React from 'react';
 import styles from './ProductPage.module.scss';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Reviews from '../../features/Reviews/Reviews';
+import ProductDetails from '../../features/ProductDetails/ProductDetails';
 
 const ProductPage = () => (
   <div className={styles.root}>
+    <ProductDetails />
     <Reviews/>
     <NewFurniture productsPerPage={4} />
   </div>
 );
+
 
 // ProductPage.propTypes = {};
 
