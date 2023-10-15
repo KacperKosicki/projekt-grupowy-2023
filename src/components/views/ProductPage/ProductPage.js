f2b3465b1faca18850d1d7d3fa5e976840d66c5c
Tomasz Cichon <tomekcichon@tlen.pl>
2023-10-15 10:18:39 +0200

    add productsPerPage prop to NewFurniture component to adjust number of products displayed

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+const ProductPage = () => (
+  <div className={styles.root}>
+    <NewFurniture productsPerPage={4} />
+  </div>
+);
 
 // ProductPage.propTypes = {};
 
