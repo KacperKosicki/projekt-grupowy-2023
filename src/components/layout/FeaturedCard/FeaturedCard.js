45c24d7aec77262aab857da4b827b705a1f063b5
Ola <cencora.aleksandra@gmail.com>
2023-10-17 18:38:55 +0200

    Add slider feature for dots

--- src/components/layout/FeaturedCard/FeaturedCard.js
+++ src/components/layout/FeaturedCard/FeaturedCard.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import styles from './FeaturedCard.module.scss';
 import { getDiscountedProducts } from '../../../redux/productsRedux';
-import { useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 const FeaturedCard = () => {
   const products = useSelector(getDiscountedProducts);
   const [currentProduct, setCurrentProduct] = useState(0);
   const [transition, setTransition] = useState(false);
+  const [autoplay, setAutoplay] = useState(true);
 
   const isFeatured = true;
 
@@ -20,16 +20,30 @@ const FeaturedCard = () => {
     }, 500);
   };
 
+  const goToSlide = index => {
+    setCurrentProduct(index);
+    addTransition();
+    setAutoplay(false);
+
+    setTimeout(() => {
+      setAutoplay(true);
+    }, 10000);
+  };
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentProduct((currentProduct + 1) % products.length);
-      addTransition();
-    }, 3000);
+    const handleAutoplay = () => {
+      if (autoplay) {
+        setCurrentProduct((currentProduct + 1) % products.length);
+        addTransition();
+      }
+    };
+
+    const interval = setInterval(handleAutoplay, 3000);
 
     return () => {
       clearInterval(interval);
     };
-  }, [currentProduct, products.length]);
+  }, [currentProduct, products.length, autoplay]);
 
   return (
     <div className={styles.featuredCard}>
@@ -37,15 +51,18 @@ const FeaturedCard = () => {
         <p>HOT DEALS</p>
         <div className={styles.dots}>
           <ul>
-            <li>
-              <a className={styles.active}>page 1</a>
-            </li>
-            <li>
-              <a>page 2</a>
-            </li>
-            <li>
-              <a>page 3</a>
-            </li>
+            {products.map((_, index) => (
+              <li key={index}>
+                <a
+                  onClick={() => goToSlide(index)}
+                  className={clsx({
+                    [styles.active]: index === currentProduct,
+                  })}
+                >
+                  {`page ${index + 1}`}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
